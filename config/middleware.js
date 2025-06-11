27a7ab7d48414a411da82130395e220375bb6388
florencioMendes <florencio.netto@setuptecnologia.com.br>
2025-06-11 11:55:54 -0300

    feat(5): added helmet and removed static files config

--- config/middleware.js
+++ config/middleware.js
@@ -1,12 +1,13 @@
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const express = require('express');
+const helmet = require('helmet');
 const path = require('path')
 
 module.exports = function (app) {
+    app.use(helmet());
     app.use(logger('dev'));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, '../', 'public')));
 }
