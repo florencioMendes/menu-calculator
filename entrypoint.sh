3442c5f0f70c05169da9787c2914abd9349d94d8
florenciomendes <florenciomendes8@gmail.com>
2025-06-14 16:53:05 -0300

    refactor(3): updated msg in script entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,17 @@ DB_HOST=${DB_MENU_HOST:-postgres}
 DB_PORT=${DB_MENU_PORT:-5432}
 APP_ENV=${APP_ENV:-development}
 
-echo 'Waiting for postgres'
-
 while ! nc -z $DB_HOST $DB_PORT; do
+  echo 'Waiting for postgres'
   sleep 3
 done
 
 echo 'Postgres started'
 
 if [ "$APP_ENV" = "production" ]; then
-  echo 'App started in prod mode'
+  echo 'App started in Production mode'
   exec npm start
 else
-  echo 'App started in dev mode'
+  echo 'App started in Development mode'
   exec npm run dev
 fi
