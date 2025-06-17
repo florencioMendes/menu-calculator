e86b7754d0db4561a720b1f2bf371300a72b7bf0
florenciomendes <florenciomendes8@gmail.com>
2025-06-17 19:51:41 -0300

    refactor(6): updated app to remove unecessary middlewares

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
-const express = require('express');
-const createError = require('http-errors');
-const routes = require('./routes');
 const middlewares = require('./middlewares');
+const routes = require('./routes');
+const express = require('express');
 require('./config');
 
 const app = express();
-
 routes(app);
+
 middlewares(app);
-
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-
-app.use(function (err, req, res) {
-    res.locals.message = err.message;
-    res.locals.error =
-        req.app.get('env') === 'development' ? err : {};
-
-    res.status(err.status || 500);
-    res.json({ title: 'error' });
-});
 
 module.exports = app;
